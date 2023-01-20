17234ed1eea6370ec81541f57eaacc863e6e36d3
Michal1898 <michal.volf@gmail.com>
2023-01-20 21:37:56 +0100

    Draw skeleton of tree.

--- draw_tree.py
+++ draw_tree.py
@@ -1,5 +1,11 @@
 import turtle
+
+turtle.mode("logo")
 turtle.speed("slowest")
+turtle.penup()
+turtle.setpos(0,-0.4*turtle.window_height())
+turtle.pendown()
+
 
 def leaf_of_tree(distance):
     turtle.forward(distance)
@@ -7,5 +13,18 @@ def leaf_of_tree(distance):
     turtle.forward(distance)
     turtle.right(180)
 
-leaf_of_tree(300)
+def branch_of_tree(distance, angle):
+    turtle.forward(distance/2)
+    turtle.right(angle)
+    leaf_of_tree(distance/2)
+    turtle.left(angle)
+    leaf_of_tree(distance/2)
+    turtle.left(angle)
+    leaf_of_tree(distance/2)
+    turtle.right(angle)
+    turtle.right(180)
+    turtle.forward(distance/2)
+
+branch_of_tree( 100, 60)
+w=input("Press Enter")
 
